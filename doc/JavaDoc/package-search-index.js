3056c7f0060a990c51f5d497a1ef2745c6ee493b
Jannik Loth <jannik.loth@gmx.de>
2024-12-06 10:25:29 +0100

    update JavaDoc

--- doc/JavaDoc/package-search-index.js
+++ doc/JavaDoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"control"},{"l":"data"},{"l":"exceptions"},{"l":"gui"},{"l":"test"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"control"},{"l":"data"},{"l":"exceptions"},{"l":"gui"},{"l":"ressources"},{"l":"test"}];updateSearchResults();
