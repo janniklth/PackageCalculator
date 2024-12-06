c2206a161af07e74d21c8a6afe5ca893d9c855e0
Jannik Loth <jannik.loth@gmx.de>
2024-12-06 10:22:15 +0100

    update JavaDoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"control"},{"l":"data"},{"l":"exceptions"},{"l":"gui"},{"l":"ressources"},{"l":"test"}];updateSearchResults();
---
+++ ressources/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides resources for the application (JSON files, constants, etc.).
+ */
+
+package ressources;
